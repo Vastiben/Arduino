ab847627098597b47fe805d2a05c039e6c9fb53b
Vastiben <98217116+Vastiben@users.noreply.github.com>
2022-01-22 15:42:03 +0100

    Create irremote.h

---
+++ librairie/irremote.h
@@ -0,0 +1,13 @@
+/**
+ * @file IRremote.h
+ *
+ * @brief Stub for backward compatibility
+ */
+
+#ifndef IRremote_h
+#define IRremote_h
+
+#include "IRremote.hpp"
+
+#endif // IRremote_h
+#pragma once
